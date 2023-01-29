ffd091e2cb024c8ba02f10924e34693585cd8ebc
Bhushan Patil <bhushan1810patil@gmail.com>
2023-01-29 18:54:55 +0530

    added settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+WIDTH = 600
+HEIGHT = 800
+GAMEWIN_WIDTH = 501
+GAMEWIN_HEIGHT = 701
+X_OFF = (WIDTH - GAMEWIN_WIDTH) // 2
+Y_OFF = int((HEIGHT - GAMEWIN_HEIGHT) * 0.8)
+FPS = 60
+TITLE_FONT = "comicsansms"
+FONT_SIZE = 40
+TITLE = "Chain Reaction"
+ROWS = 14
+COLS = 10
+
+# simple grid: 11 * 6
+# hd grid: 19 * 10
